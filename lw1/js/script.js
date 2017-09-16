d356bb28c5c9044519564f6009a8ff8fcc9980e8
SkurihinP <p.elkin2017@yandex.ru>
2017-09-16 14:34:56 +0400

    6 пункт выполнен
    Изменение в файле js

--- lw1/js/script.js
+++ lw1/js/script.js
@@ -1,28 +1,24 @@
 function getRandomInt(min, max) { 
-    return Math.floor(Math.random() * (max - min + 1)) + min 
-    }
+  return Math.floor(Math.random() * (max - min + 1)) + min 
+  }
+
 var N = 10, a = [N], k,i, isFound;
 
 for (i=1;i<N;i++)
 {
-    a[i] = getRandomInt(1,15);
+  a[i] = getRandomInt(0,15);
 }
-
-
 alert ("Элементы массива: "+ a.join(" "));
-k = getRandomInt(1,95);
-alert ("Элемент К= "+k);
+var k = prompt("Введите число К: ",'');
 isFound = false;
-
 for (i=1;i<N;i++)
 {
-    if (a[i] = k) 
+  if (a[i] == k) 
     {
-        isFound = true;
+      isFound = true;
     }
-    else isFound = false;
+  
 }
-
 if (isFound) {
   alert('Элемент найден')
 }
