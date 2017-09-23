df336828643aeff99ef17ef880c9d806a88780d3
SkurihinP <p.elkin2017@yandex.ru>
2017-09-23 12:48:09 +0400

    Отключил css и удалил файл за ненадобностью

--- lw1/js/script.js
+++ lw1/js/script.js
@@ -2,32 +2,29 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min 
   }
 
-var POSITIONS = 10, mass = [POSITIONS], k,i, isFound;
+var POSITIONS = 10;
+var mass = [POSITIONS];
+var k,i, isFound;
 
-for (i=1;i<POSITIONS;i++)
+for (i = 0; i < POSITIONS; i++)
 {
   mass[i] = getRandomInt(0,15);
 }
 alert ("Элементы массива: "+ mass.join(" "));
 var k = prompt("Введите число К: ",'');
-if ((k=="") || (k==null)|| (isNaN(k)))
-{
-    alert("Ошибка");
+if ((k=="") || (k==null) || (isNaN(k))) {
+    alert ("Ошибка");
     break;
 }
 isFound = false;
-for (i=1;i<POSITIONS;i++)
-{
-  if (mass[i] == k) 
-    {
+for (i = 0; i < POSITIONS; i++) {
+  if (mass[i] == k) {
       isFound = true;
     }
-  
 }
 if (isFound) {
-  alert('Элемент найден')
+  alert('Элемент найден');
 }
-else
-{
-  alert('Элемент не найден!')
+else {
+  alert('Элемент не найден!');
 }
