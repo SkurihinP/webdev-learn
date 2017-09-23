81cac8e60ff56b44245b1bd58ee3ae5f066c70ec
SkurihinP <p.elkin2017@yandex.ru>
2017-09-23 12:57:23 +0400

    Назвал перменные смысловыми именами

--- lw1/js/script.js
+++ lw1/js/script.js
@@ -1,24 +1,23 @@
 function getRandomInt(min, max) { 
   return Math.floor(Math.random() * (max - min + 1)) + min 
   }
-
 var POSITIONS = 10;
 var mass = [POSITIONS];
-var k,i, isFound;
+var i, isFound;
 
 for (i = 0; i < POSITIONS; i++)
 {
   mass[i] = getRandomInt(0,15);
 }
-alert ("Элементы массива: "+ mass.join(" "));
-var k = prompt("Введите число К: ",'');
-if ((k=="") || (k==null) || (isNaN(k))) {
+alert ("Элементы массива: " + mass.join(" "));
+var timeVariable = prompt("Введите число К: ",'');
+if ((timeVariable == "") || (timeVariable == null) || (isNaN(timeVariable))) {
     alert ("Ошибка");
     break;
 }
 isFound = false;
 for (i = 0; i < POSITIONS; i++) {
-  if (mass[i] == k) {
+  if (mass[i] == timeVariable) {
       isFound = true;
     }
 }
