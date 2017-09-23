aee633d73af2ee3d6f08eed9df476f1ca56d2181
SkurihinP <p.elkin2017@yandex.ru>
2017-09-23 14:46:20 +0400

    Переименовал переменные в смысловые, форматировал, удалил ненужный код

--- lw1/js/search_arr_elements.js
+++ lw1/js/search_arr_elements.js
@@ -1,29 +1,23 @@
 function getRandomInt(min, max) { 
   return Math.floor(Math.random() * (max - min + 1)) + min;
   }
-var POSITION = 10;
-var mass = [POSITION];
-var i,timeVariable, isFound;
-for (i = 0; i < POSITION; i++)
-{
-  mass[i] = getRandomInt(0, 15);
+var POSITION_COUNT = 10;
+var arr = [];
+var i, timeVariable;
+
+for (i = 0; i < POSITION_COUNT; i++) {
+  arr[i] = getRandomInt(0, 15);
 }
-alert ("Элементы массива: " + mass.join(" "));
+alert ("Элементы массива: " + arr.join(" ") );
 do {
   timeVariable = prompt("Введите число: ", '');
   timeVariable = parseInt(timeVariable);
-  if ((timeVariable == "") || (isNaN(timeVariable))) {
+  if ( (timeVariable == "") || (isNaN(timeVariable) ) ) {
       alert ("Ошибка");
   }
 }
-while ((timeVariable == "") || (isNaN(timeVariable)))
-isFound = false;
-for (i = 0; i < POSITION; i++) {
-  if (mass[i] == timeVariable) {
-      isFound = true;
-    }
-}
-if (mass.indexOf(timeVariable) !== -1) {
+while ( (timeVariable == "") || (isNaN(timeVariable) ) );
+if (arr.indexOf(timeVariable) !== -1) {
   alert("Элемент найден");
 }
 else {
